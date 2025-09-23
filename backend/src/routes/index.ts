599320a4b2192add03da21a99d27d2d24bcc6b1f
ShaswataG <gshaswata1@gmail.com>
2025-09-24 02:51:45 +0530

    feat(poll): setup poll route skeletons

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,8 @@
 const express = require('express');
+const pollRoutes = require('./poll.routes');
 
 const router = express.Router();
+
+router.use('/polls', pollRoutes);
 
 module.exports = router;
