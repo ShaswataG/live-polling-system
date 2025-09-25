b9a746c007595ee2cc4f4808e7ea0fb9e20a3239
ShaswataG <gshaswata1@gmail.com>
2025-09-26 01:15:16 +0530

    api base url

--- frontend/src/components/custom/global/IntervuePollBadge.tsx
+++ frontend/src/components/custom/global/IntervuePollBadge.tsx
@@ -1,10 +1,11 @@
 import { Badge } from "@/components/ui/badge";
+import intervuePollIcon from 'src/assets/home/intervue-poll-icon.svg';
 
 export default function BadgeComponent() {
     return (
         <Badge variant="default" className="bg-gradient-to-r from-[#7565D9] to-[#4D0ACD] rounded-xl">
             <span>
-                <img src="src/assets/home/intervue-poll-icon.svg" alt="icon" />
+                <img src={intervuePollIcon} alt="icon" />
             </span>
             <span className="text-sm">Intervue Poll</span>
         </Badge>
